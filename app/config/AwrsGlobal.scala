10a9a4dc2b25fecb51d4b20e58a397a4fa6160d6
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2018-01-04 15:10:44 +0000

    Change to readme to allow build on jenkins

--- app/config/AwrsGlobal.scala
+++ app/config/AwrsGlobal.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -61,4 +61,3 @@ object AwrsGlobal extends DefaultMicroserviceGlobal with RunMode {
   override def authFilter = Some(MicroserviceAuthFilter)
 
 }
-
