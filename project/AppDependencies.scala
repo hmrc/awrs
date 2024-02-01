9cdc0143431ec1adf6153f01dc37eacf7d0229cf
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2024-02-01 11:04:19 +0000

    DL-12466 dependency updates applied

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,19 +2,18 @@ import sbt._
 
 object AppDependencies {
   import play.sbt.PlayImport._
-  import play.core.PlayVersion
 
   private val domainVersion = "9.0.0"
-  private val json4sJacksonVersion = "4.0.6"
+  private val json4sJacksonVersion = "4.0.7"
   private val jsonSchemaValidatorVersion = "2.2.14"
-  private val json4sNativeVersion = "4.0.6"
-  private val mockitoScalatestVersion = "1.17.14"
+  private val json4sNativeVersion = "4.0.7"
+  private val mockitoScalatestVersion = "1.17.30"
   private val microserviceBootstrapVersion = "8.4.0"
-  private val jsoupVersion = "1.16.1"
-  private val wiremockJre8Version = "2.33.2"
-  private val jacksonModuleVersion = "2.15.2"
+  private val jsoupVersion = "1.17.2"
+  private val wiremockVersion = "3.3.1"
+  private val jacksonModuleVersion = "2.16.1"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"   % microserviceBootstrapVersion,
     "uk.gov.hmrc"                   %% "domain-play-30"              % domainVersion,
@@ -32,9 +31,9 @@ object AppDependencies {
   object Test {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test: Seq[ModuleID] = Seq(
-        "uk.gov.hmrc"                   %% "bootstrap-test-play-28"   % microserviceBootstrapVersion    % scope,
-        "org.jsoup"                     %  "jsoup"                    % jsoupVersion                    % scope,
-        "org.mockito"                   %% "mockito-scala-scalatest"  % mockitoScalatestVersion         % scope
+        "uk.gov.hmrc" %% "bootstrap-test-play-28"  % microserviceBootstrapVersion % scope,
+        "org.jsoup"   %  "jsoup"                   % jsoupVersion                 % scope,
+        "org.mockito" %% "mockito-scala-scalatest" % mockitoScalatestVersion      % scope
       )
     }.test
   }
@@ -42,9 +41,9 @@ object AppDependencies {
   object IntegrationTest {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val scope: String = "it"
-      override lazy val test = Seq(
-        "uk.gov.hmrc"                   %% "bootstrap-test-play-28"   % microserviceBootstrapVersion    % scope,
-        "com.github.tomakehurst"        %  "wiremock-jre8"            % wiremockJre8Version             % scope
+      override lazy val test: Seq[sbt.ModuleID] = Seq(
+        "uk.gov.hmrc"  %% "bootstrap-test-play-28" % microserviceBootstrapVersion % scope,
+        "org.wiremock" %  "wiremock"               % wiremockVersion              % scope
       )
     }.test
   }
