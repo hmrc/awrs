e02656fc41b9bc3de0fefc32151c9fdf77ccbf88
christianmennerich <177663202+christianmennerich@users.noreply.github.com>
2025-12-12 11:55:48 +0000

    DL-18033: EtmpRegimeService changes
    activates regimeCheck in test
    
    adds tests for 422 responses
    * code 003 - no ID found is equivalent to not found
    * everything else is condidered an error
    
    makes HIP route to checkStatus available in EtmpRegimeService
    * add the route to checkStatuts on HipConnector to the service if the
      HIP feature flag is enabled
    * add tests to the service for all calls to the checkStatus on
      HipConnector that existed for calls to checkStatus on EtmpConnector, to
      ensure behaviour stays stable when changing the APIs

--- app/services/EtmpRegimeService.scala
+++ app/services/EtmpRegimeService.scala
@@ -16,26 +16,32 @@
 
 package services
 
-import connectors.{EnrolmentStoreConnector, EtmpConnector}
+import connectors.{EnrolmentStoreConnector, EtmpConnector, HipConnector}
+
 import javax.inject.Inject
 import models._
 import play.api.Logging
 import play.api.http.Status._
+import play.api.libs.json.{JsString, Json}
 import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
 import uk.gov.hmrc.auth.core.{AffinityGroup, AuthConnector, AuthorisedFunctions, User}
 import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
-import utils.AWRSFeatureSwitches
+import utils.{AWRSFeatureSwitches, Utility}
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
 class EtmpRegimeService @Inject()(etmpConnector: EtmpConnector,
+                                  hipConnector: HipConnector,
                                   val enrolmentStoreConnector: EnrolmentStoreConnector,
                                   val authConnector: AuthConnector) extends AuthorisedFunctions
                                   with NonSelfHealStatus
                                   with Logging {
 
   private val AWRS_SERVICE_NAME = "HMRC-AWRS-ORG"
+
+  private val errorsNode = "errors"
+  private val codeNode = "code"
 
   def getEtmpBusinessDetails(safeId: String)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Option[EtmpRegistrationDetails]] = {
     etmpConnector.awrsRegime(safeId).map { response =>
@@ -83,15 +89,31 @@ class EtmpRegimeService @Inject()(etmpConnector: EtmpConnector,
   }
 
   def fetchETMPStatus(refNoNumber: String)(implicit ec: ExecutionContext, hc: HeaderCarrier): Future[Option[String]] = {
-    etmpConnector.checkStatus(refNoNumber) map { response =>
-      response.status match {
-        case OK =>
-          val statusType = response.json.as[SubscriptionStatusType](SubscriptionStatusType.reader)
-          Some(statusType.formBundleStatus.name)
-        case NOT_FOUND => None
-        case status =>
-          logger.warn(s"[EtmpRegimeService][fetchETMPStatus] Failed to check ETMP API9 status: $status")
-          throw new RuntimeException(s"[EtmpRegimeService][fetchETMPStatus] Failed to check ETMP API9 status: $status")
+
+    if (AWRSFeatureSwitches.hipSwitch().enabled) {
+      hipConnector.checkStatus(refNoNumber) map { response =>
+        response.status match {
+          case OK =>
+            val statusType = Utility.stripSuccessNode(response.json).as[SubscriptionStatusType](SubscriptionStatusType.reader)
+            Some(statusType.formBundleStatus.name)
+          case NOT_FOUND => None
+          case UNPROCESSABLE_ENTITY if hipErrorCode(response.body) == Some("002") => None
+          case status =>
+            logger.warn(s"[EtmpRegimeService][fetchETMPStatus] Failed to check ETMP API9 status: $status ${response.body}")
+            throw new RuntimeException(s"[EtmpRegimeService][fetchETMPStatus] Failed to check ETMP API9 status: $status ${response.body}")
+        }
+      }
+    } else {
+      etmpConnector.checkStatus(refNoNumber) map { response =>
+        response.status match {
+          case OK =>
+            val statusType = response.json.as[SubscriptionStatusType](SubscriptionStatusType.reader)
+            Some(statusType.formBundleStatus.name)
+          case NOT_FOUND => None
+          case status =>
+            logger.warn(s"[EtmpRegimeService][fetchETMPStatus] Failed to check ETMP API9 status: $status")
+            throw new RuntimeException(s"[EtmpRegimeService][fetchETMPStatus] Failed to check ETMP API9 status: $status")
+        }
       }
     }
   }
@@ -191,6 +213,16 @@ class EtmpRegimeService @Inject()(etmpConnector: EtmpConnector,
       case Some(AffinityGroup.Individual) if matchIndividual(bcd, erd) => Some(erd)
       case Some(AffinityGroup.Organisation) if matchOrg(bcd, erd) => Some(erd)
       case _ => None
+    }
+  }
+
+  private def hipErrorCode(jsonString: String): Option[String] = {
+
+    for {
+      errorMessageBody <- Try(Json.parse(jsonString)).toOption
+      errorCodeNode <- (errorMessageBody \ errorsNode \ codeNode).toOption
+    } yield {
+      errorCodeNode.asInstanceOf[JsString].value
     }
   }
 }
