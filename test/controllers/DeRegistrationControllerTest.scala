ef33889abaa670fa511f62c36d321d6a765be044
Byasar3 <byasar3@users.noreply.github.com>
2025-12-30 16:18:29 +0000

    DL-18012: added 422 error handling to DeRegistrationController.scala

--- test/controllers/DeRegistrationControllerTest.scala
+++ test/controllers/DeRegistrationControllerTest.scala
@@ -108,5 +108,130 @@ class DeRegistrationControllerTest extends BaseSpec with AnyWordSpecLike {
       status(result) shouldBe INTERNAL_SERVER_ERROR
     }
 
+    "return NOT_FOUND for 422 with HIP code 002" in {
+
+      val error002 = """{
+                       |  "errors": {
+                       |    "processingDate": "2025-12-02T13:14:41Z",
+                       |    "code": "002",
+                       |    "text": "No records found"
+                       |  }
+                       |}
+                       |  """.stripMargin
+
+      when(mockDeRegistrationService.deRegistration(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any()))
+        .thenReturn(Future.successful(HttpResponse(UNPROCESSABLE_ENTITY, error002, Map.empty[String, Seq[String]])))
+      val result = TestDeRegistrationControllerTest.deRegistration(testRefNo).apply(FakeRequest().withJsonBody(api10RequestJson))
+      status(result) shouldBe NOT_FOUND
+      contentAsJson(result) shouldBe Json.parse(error002)
+    }
+
+    "return BAD_REQUEST for 422 with HIP code 003" in {
+
+      val error003 = """{
+                       |  "errors": {
+                       |    "processingDate": "2025-12-02T13:14:41Z",
+                       |    "code": "003",
+                       |    "text": "Invalid deregistration reason"
+                       |  }
+                       |}
+                       |  """.stripMargin
+
+      when(mockDeRegistrationService.deRegistration(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any()))
+        .thenReturn(Future.successful(HttpResponse(UNPROCESSABLE_ENTITY, error003, Map.empty[String, Seq[String]])))
+      val result = TestDeRegistrationControllerTest.deRegistration(testRefNo).apply(FakeRequest().withJsonBody(api10RequestJson))
+      status(result) shouldBe BAD_REQUEST
+      contentAsJson(result) shouldBe Json.parse(error003)
+    }
+
+    "return BAD_REQUEST for 422 with HIP code 004" in {
+
+      val error004 = """{
+                       |  "errors": {
+                       |    "processingDate": "2025-12-02T13:14:41Z",
+                       |    "code": "004",
+                       |    "text": "Deregistration date is in the future"
+                       |  }
+                       |}
+                       |  """.stripMargin
+
+      when(mockDeRegistrationService.deRegistration(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any()))
+        .thenReturn(Future.successful(HttpResponse(UNPROCESSABLE_ENTITY, error004, Map.empty[String, Seq[String]])))
+      val result = TestDeRegistrationControllerTest.deRegistration(testRefNo).apply(FakeRequest().withJsonBody(api10RequestJson))
+      status(result) shouldBe BAD_REQUEST
+      contentAsJson(result) shouldBe Json.parse(error004)
+    }
+
+    "return BAD_REQUEST for 422 with HIP code 005" in {
+
+      val error005 =
+        """{
+          |  "errors": {
+          |    "processingDate": "2025-12-02T13:14:41Z",
+          |    "code": "005",
+          |    "text": "Deregistration date is before registration date"
+          |  }
+          |}
+          |  """.stripMargin
+
+      when(mockDeRegistrationService.deRegistration(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any()))
+        .thenReturn(Future.successful(HttpResponse(UNPROCESSABLE_ENTITY, error005, Map.empty[String, Seq[String]])))
+      val result = TestDeRegistrationControllerTest.deRegistration(testRefNo).apply(FakeRequest().withJsonBody(api10RequestJson))
+      status(result) shouldBe BAD_REQUEST
+      contentAsJson(result) shouldBe Json.parse(error005)
+    }
+
+    "return INTERNAL_SERVER_ERROR for 422 with HIP code 999" in {
+
+      val error999 =
+        """{
+          |  "errors": {
+          |    "processingDate": "2025-12-02T13:14:41Z",
+          |    "code": "999",
+          |    "text": "Internal server error"
+          |  }
+          |}
+          |  """.stripMargin
+
+      when(mockDeRegistrationService.deRegistration(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any()))
+        .thenReturn(Future.successful(HttpResponse(UNPROCESSABLE_ENTITY, error999, Map.empty[String, Seq[String]])))
+      val result = TestDeRegistrationControllerTest.deRegistration(testRefNo).apply(FakeRequest().withJsonBody(api10RequestJson))
+      status(result) shouldBe INTERNAL_SERVER_ERROR
+    }
+
+    "return INTERNAL_SERVER_ERROR for 422 with unknown HIP code" in {
+
+      val errorUnknown =
+        """{
+          |  "errors": {
+          |    "processingDate": "2025-12-02T13:14:41Z",
+          |    "code": "123",
+          |    "text": "Unknown error"
+          |  }
+          |}
+          |  """.stripMargin
+
+      when(mockDeRegistrationService.deRegistration(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any()))
+        .thenReturn(Future.successful(HttpResponse(UNPROCESSABLE_ENTITY, errorUnknown, Map.empty[String, Seq[String]])))
+      val result = TestDeRegistrationControllerTest.deRegistration(testRefNo).apply(FakeRequest().withJsonBody(api10RequestJson))
+      status(result) shouldBe INTERNAL_SERVER_ERROR
+    }
+
+    "return INTERNAL_SERVER_ERROR for 422 with missing hip code" in {
+
+      val errorMissingCode =
+        """{
+          |  "errors": {
+          |    "processingDate": "2025-12-02T13:14:41Z",
+          |    "text": "Missing code"
+          |  }
+          |}
+          |  """.stripMargin
+
+      when(mockDeRegistrationService.deRegistration(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any()))
+        .thenReturn(Future.successful(HttpResponse(UNPROCESSABLE_ENTITY, errorMissingCode, Map.empty[String, Seq[String]])))
+      val result = TestDeRegistrationControllerTest.deRegistration(testRefNo).apply(FakeRequest().withJsonBody(api10RequestJson))
+      status(result) shouldBe INTERNAL_SERVER_ERROR
+    }
   }
 }
